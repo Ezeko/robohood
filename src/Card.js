6853abcd7f15749c37a03f602bc3da241865c560
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-08-11 18:49:03 +0100

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({id, name, email})=>{
-    return(
-        <div>
+    /*another way to write this is by doing destructuring
+    const Card =({props}){
+        const {id, name, email} = props;
+        return(   
+            <div className='main'>
+            <div className='container'>
             <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <p>{name}</p>
             <p>{email}</p>
+            </div>
+        </div>);
+    }
+    */
+    return(
+        <div className='main'>
+            <div className='container'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <p>{name}</p>
+            <p>{email}</p>
+            </div>
         </div>
     );
 }
