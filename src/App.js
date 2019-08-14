44cea9610713233e130f8f9b99e18dcfc249436e
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-08-15 00:57:23 +0100

    Update App.js
    parent component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import important and revelevant components here
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// Create class here because this is the parent component of the app
+class App extends Component {
+    //constructor as every componet has
+    constructor(){
+    super()
+    // declare the states. the state are the dynamic variables of an app
+    this.state ={
+        robots: robots,
+        SearchField: ''
+    }}
+
+    //instance  where the state variable is changed
+     Searched = (event)=>{
+            this.setState({SearchField:event.target.value});
+            
+            }
+    
+    
+    // this is what is displayed on the app
+    render(){ 
+        const filteredRobots = this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.SearchField.toLowerCase());})      
+     return(
+        <div className="app">
+            <h1>Robofriends</h1>
+            <SearchBox SearchChange = {this.Searched} />
+            <hr />
+            <CardList robots = {filteredRobots} />
+        </div>
+        );
+    }}
+
 
 export default App;
