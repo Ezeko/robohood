2ab140c81bfa15627e38233657fb0c1350974ab4
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-01-23 11:29:00 +0100

    Updates

---
+++ precache-manifest.e96fce1f23d013c3e6250776b5c32765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b695572318adbba5d6499946b339063",
+    "url": "/robohood/index.html"
+  },
+  {
+    "revision": "8ed0c784a2ee800adab6",
+    "url": "/robohood/static/css/main.bbf32c9a.chunk.css"
+  },
+  {
+    "revision": "94266ca5c6c91af3cdea",
+    "url": "/robohood/static/js/2.12a3f50c.chunk.js"
+  },
+  {
+    "revision": "8ed0c784a2ee800adab6",
+    "url": "/robohood/static/js/main.7f92d4fa.chunk.js"
+  },
+  {
+    "revision": "734e0ae40e7b712273f7",
+    "url": "/robohood/static/js/runtime~main.543fdbe8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.466140ebac1432a47aa6354505ed5be1.js"
+  "/robohood/precache-manifest.e96fce1f23d013c3e6250776b5c32765.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robohood/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
